ca1c5cecacad114e74b54552f3d5191393d6a787
Angel-B113 <angel.ricahalo3@gmail.com>
2026-04-06 19:34:44 -0600

    Moved MAX_NODES constant to rb_tree_test.c

--- include/rb_tree_test.h
+++ include/rb_tree_test.h
@@ -3,11 +3,9 @@
 
 #include "../include/base.h"
 
-#define MAX_NODES 1000
-
 extern bool tree_test_insertion ( u32 nodes );
 extern bool tree_test_deletion ( u32 nodes );
 extern bool tree_test ( void );
-extern void tree_test_performance ( u32 insert_nodes, u32 deleted_nodes );  
+extern void tree_test_performance ( void );  
 
 #endif

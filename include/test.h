fb356dc101a31c62dca28d8d6c303099d782a426
Angel-B113 <angel.ricahalo3@gmail.com>
2026-04-13 17:41:16 -0600

    Test suite changes: including CTest

--- include/test.h
+++ include/test.h
@@ -3,13 +3,13 @@
 
 #include "../include/base.h"
 
-extern bool general_test ( void );
+extern bool general_test(void);
 
-extern bool tree_test ( void );
+extern bool tree_test(void);
 
-extern bool test_malloc ( void );
-extern bool test_realloc ( void );
-extern bool test_free ( void );
-extern bool test_fragmentation ( void ); 
+extern bool test_malloc(void);
+extern bool test_realloc(void);
+extern bool test_free(void);
+extern bool test_fragmentation(void);
 
 #endif
--- test/test.c
+++ test/test.c
@@ -1,13 +1,55 @@
-#include "../include/rb_tree_test.h"
 #include "../include/allocator_test.h"
+#include "../include/rb_tree_test.h"
+#include <string.h>
 
-void general_test ( void ) {    
-    bool test = tree_test();
-    if ( test ) tree_test_performance();
-    example();     
+#ifndef MAX_NODES
+#define MAX_NODES 1000000
+#endif
+
+#ifndef NODES_TO_INSERT
+#define NODES_TO_INSERT 750000
+#endif
+
+#ifndef NODES_TO_DELETE
+#define NODES_TO_DELETE 500000
+#endif
+
+static bool gnrltree_test(void);
+static bool tree_testing(const char *test_type);
+static bool allocator_testing(const char *test_type);
+
+int main(int argc, char **argv) {
+  if (argc < 1) {
+    puts("Must select a module to test");
+    return 0;
+  }
+  if (!strcmp(argv[0], "tree"))
+    return tree_testing(argv[1]);
+  else if (!strcmp(argv[0], "allocator"))
+    return allocator_testing(argv[1]);
+  else
+    puts("Selection is invalid");
 }
 
-int main( void ) {
-    example();  
-    return 0; 
+static bool gnrltree_test(void) {
+  bool ans = tree_test();
+  if (ans)
+    tree_test_performance();
+  return ans;
 }
+
+static bool tree_testing(const char *test_type) {
+  if (!strcmp(test_type, "general"))
+    return gnrltree_test();
+  else if (!strcmp(test_type, "insertion"))
+    return tree_test_insertion(NODES_TO_INSERT);
+  else if (!strcmp(test_type, "deletion"))
+    return tree_test_deletion(NODES_TO_DELETE);
+  else if (!strcmp(test_type, "performance"))
+    tree_test_performance();
+  else
+    puts("Selection is invalid");
+  return 0;
+}
+
+static bool allocator_testing(const char *test_type) { return 0; }
